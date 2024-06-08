17161669698988a899257e29ef260ac7114ade5d
Pedro Augusto Campos Dos Santos <p.augustocampos@gmail.com>
2024-06-07 21:49:42 -0300

    refactor: rename example gopkg

---
+++ extras/0_sample/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+// sample
+// https://leetcode.com/problems/missing-number/
+func sample(nums []int) int {
+	return -1
+}
+
+func main() {
+	fmt.Println(sample([]int{})) // [2] 2.5
+}
+
+// func Example() {
+// 	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+// 	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6}))
+// 	// Output:
+// 	// 6
+// 	// 5
+// }
+
+// func Example_foo() {
+// 	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6}))
+// 	// Output: 5
+// }
